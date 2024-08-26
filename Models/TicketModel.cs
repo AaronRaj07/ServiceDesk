357a3fbe698362072d37db6b5a16554c20505095
Aaron <jadram266@gmail.com>
2024-08-26 13:56:30 +0800

    Fixed constraint issue which will reflect in the database

--- Models/TicketModel.cs
+++ Models/TicketModel.cs
@@ -1,14 +1,22 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
 namespace ServiceDesk.Models
 {
     public class Ticket
     {
         public int Id { get; set; }
-        public string? Title { get; set; }
-        public string? Description{ get; set; }
-        public string? Status { get; set; }
+
+        [Required(ErrorMessage = "Title of the ticket is required!")]
+        public string Title { get; set; } = string.Empty;
+        public string? Description{ get; set; } = string.Empty;
+        public string? Status { get; set; } = string.Empty;
         public DateTime CreatedDate { get; set; }
         public DateTime ResolvedDate { get; set; }
-        //public string? RequestorName { get; set; }
-        //public string ResolverName { get; set; }
+
+        [Required(ErrorMessage = "Add the requestor name of this ticket")]
+        [Display(Name = "Requestor Name")]
+        public string RequestorName { get; set; } = string.Empty;
+        public string ResolverName { get; set; } = string.Empty;
     }
 }
